72cf53b660b290d0928e23193c1ef72ecd20ffec
Ivan V. Ivanov <ivanov.biol@gmail.com>
2021-04-29 09:55:16 +0300

    Task updated

--- java-oop/3-inheritance/single-inheritance/Dog.java
+++ java-oop/3-inheritance/single-inheritance/Dog.java
@@ -5,6 +5,11 @@ public class Dog extends Animal {
     public Dog() {
     }
 
+    @Override
+    protected void eat() {
+        super.eat();
+    }
+
     public void bark() {
         System.out.println(EATING_MESSAGE);
     }
