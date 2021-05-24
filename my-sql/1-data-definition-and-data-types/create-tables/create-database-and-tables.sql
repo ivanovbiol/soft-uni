bb7745b045224fd9c2ad034d03ecb49177cb5ad7
Ivan V. Ivanov <ivanov.biol@gmail.com>
2021-05-24 13:20:26 +0300

    Task updated

--- my-sql/1-data-definition-and-data-types/create-tables/create-database-and-tables.sql
+++ my-sql/1-data-definition-and-data-types/create-tables/create-database-and-tables.sql
@@ -2,12 +2,12 @@ CREATE DATABASE `temporary`;
 
 CREATE TABLE `employees` (
 	`id` INT PRIMARY KEY AUTO_INCREMENT,
-    `fist_name` VARCHAR(33) NOT NULL,
+    `first_name` VARCHAR(33) NOT NULL,
     `last_name` VARCHAR(33) NOT NULL
 );
 
 CREATE TABLE `products` (
 	`id` INT PRIMARY KEY AUTO_INCREMENT,
     `name` VARCHAR(33) NOT NULL
-);
+);DROP DATABASE temporary;
 
