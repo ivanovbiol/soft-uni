4a69b75d8cf3f3528873b660433c95f6e9574903
Ivan V. Ivanov <ivanov.biol@gmail.com>
2021-05-25 10:29:46 +0300

    Task solved

---
+++ my-sql/1-data-definition-and-data-types/insert-records-in-both-tables-exercise/insert-records-in-both-tables.sql
@@ -0,0 +1,17 @@
+INSERT INTO `towns`
+VALUES
+(1, 'Sofia'),
+(2, 'Plovdiv'),
+(3, 'Varna');
+
+-- To view the records
+SELECT * FROM `minions`.`towns`;
+
+INSERT INTO `minions`
+VALUES
+(1, 'Pesho', 15, 1),
+(2, 'Ivan', 29, 2),
+(3, 'Gosho', 34, 3);
+
+-- To view the records
+SELECT * FROM `minions`.`minions`;
